e9ffec0a36b68d7fd2f6c545ca744a1bb57221ba
kalyan <kalyanramu@gmail.com>
2013-03-22 16:01:24 -0500

    Adding FlexRIO Host Library

---
+++ githubpush.bat
@@ -0,0 +1,16 @@
+:: Configure File Source Location
+cd 'C:\Users\kvemishe\Documents\HHMI\Phase1'
+git init
+git status
+git add .
+
+:: Commit to Git and Give a Comment
+git commit -m "Adding FlexRIO Host Library"
+git status
+
+:: Configure Git Hub Destination Location on Web and mae it as ORIGIN
+git remote add origin https://github.com/kalyanramu/5734Code.git
+
+:: Push to GitHub Repository on Web
+git push -u origin master
+
