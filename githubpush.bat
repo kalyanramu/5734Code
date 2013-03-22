8d2d33cb287f90c0cd91d87aa4421fc0fcbbf920
kalyan <kalyanramu@gmail.com>
2013-03-22 17:33:27 -0500

    Adding FlexRIO Host Library

--- githubpush.bat
+++ githubpush.bat
@@ -11,7 +11,7 @@ git status
 :: Configure Git Hub Destination Location on Web and mae it as ORIGIN
 ::git remote add origin https://github.com/kalyanramu/5734Code.git
 ::git remote set-url origin git@github.com/kalyanramu/5734Code.git
-git remote add origin2 git@github.com/kalyanramu/5734Code.git
+git remote add origin2 git@github.com:kalyanramu/5734Code.git
 pause
 ::Get Remote Location Info
 ::git config --get-regexp '^(remote|branch)\.'
